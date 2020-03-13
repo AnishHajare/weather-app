c681435aa1348635da5a2f7fce475ab7a6bb3ec6
AnishHajare <anish.hajare9@gmail.com>
2020-03-13 12:31:53 +0530

    Units changed to SI

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/94fcfb3eeae985ba4cda2df99bcacb19/' + latitude + ',' + longitude + '?units=auto'
+    const url = 'https://api.darksky.net/forecast/94fcfb3eeae985ba4cda2df99bcacb19/' + latitude + ',' + longitude +'?units=si'
 
     request({ url, json: true }, (error,{ body }) => {
         if (error) {
